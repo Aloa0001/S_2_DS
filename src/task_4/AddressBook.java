62b0d518dbfcf3a2d90f9499ce198961739de460
Aloa0001 <alex.oachesu0001@stud.hkr.se>
2019-12-01 14:40:15 +0100

    updates

--- src/task_4/AddressBook.java
+++ src/task_4/AddressBook.java
@@ -2,36 +2,41 @@ package task_4;
 
 public class AddressBook {
 
-    private  MyLinkedListT4<Person> addressBook;
-    private int size;
-
+    private  MyLinkedListT4<Person> addressBook; // will store all the people
+    /**
+     * Constructor
+     */
     public AddressBook(){
         this.addressBook = new MyLinkedListT4<>();
-        this.size = addressBook.size();
     }
-
+    /**
+     * getter
+     */
     public MyLinkedListT4<Person> getAddressBook() {
         return addressBook;
     }
-
+    /**
+     * returns the number of people in the address book
+     */
     public int getSize() {
         return addressBook.size();
     }
-
+    /**
+     * adds at the and off the address book a new person item
+     */
     public void addNewNameAndAddress(Person person){
         addressBook.addLast(person);
     }
+    /**
+     * removes a person item using the linked list remove(T obj) method
+     */
     public void remove(Person person){
         addressBook.remove(person);
     }
+    /**
+     * returns the person obj at index
+     */
     public Person getPerson(int index){
         return addressBook.get(index);
-    }
-
-    @Override
-    public String toString() {
-        return "AddressBook{" +
-                "addressBook=" + addressBook +
-                '}';
     }
 }

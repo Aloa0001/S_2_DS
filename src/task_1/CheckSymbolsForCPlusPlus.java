9e97465425c9302e2b8e10506820d7eefb316cbe
Aloa0001 <alex.oachesu0001@stud.hkr.se>
2019-12-03 15:35:27 +0100

    task 1 updates

--- src/task_1/CheckSymbolsForCPlusPlus.java
+++ src/task_1/CheckSymbolsForCPlusPlus.java
@@ -3,24 +3,24 @@ package task_1;
 import java.util.Stack;
 
 public class CheckSymbolsForCPlusPlus {
-    public static String balanceChecking(String[] code) {
-        String valid = "Valid";
-        for (String line : code) {
-            if (valid.equals("Valid")) {
-                Stack<Character> active = new Stack<>();
-                Stack<Character> cancel = new Stack<>();
-                char[] words = line.toCharArray();
-                fillStacks(words, active, cancel);
-                checkStacks(active, cancel, valid);
-                if (!active.isEmpty() || !cancel.isEmpty()) {
-                    valid = "NOT Valid";
-                }
-            } else {
-                break;
-            }
-        }
-        return valid;
-    }
+//    public static String balanceChecking(String[] code) {
+//        String valid = "Valid";
+//        for (String line : code) {
+//            if (valid.equals("Valid")) {
+//                Stack<Character> active = new Stack<>();
+//                Stack<Character> cancel = new Stack<>();
+//                char[] words = line.toCharArray();
+//                fillStacks(words, active, cancel);
+//                checkStacks(active, cancel, valid);
+//                if (!active.isEmpty() || !cancel.isEmpty()) {
+//                    valid = "NOT Valid";
+//                }
+//            } else {
+//                break;
+//            }
+//        }
+//        return valid;
+//    }
     public static String balanceChecking(String code) {
         String valid = "Valid";
         for (int i = 0; i < code.length(); i++) {
@@ -70,9 +70,10 @@ public class CheckSymbolsForCPlusPlus {
     }
 
     public static void main(String[] args) {
-        String[] testValid = {"1. if(a == b) {a++;} ", "2. if(a < (b*c)) {t = 5;} ",
-                "3. int []b = new int[5];  ", "4. [](){} 5. int a = 5; // init a to 5 "};
-        String[] testNonValid = {"1. for(int i=0;i<10;i++] {a+= b;} ", " 2. {abc) "};
+        String testValid = "1. if(a == b) {a++;} 2. if(a < (b*c)) {t = 5;} 3. int []b = new int[5];  4. [](){} 5. int a = 5; // init a to 5 ";
+        String testNonValid = "1. for(int i=0;i<10;i++] {a+= b;}  2. {abc) ";
+//        System.out.println(balanceChecking(testValid));
+//        System.out.println(balanceChecking(testNonValid));
         System.out.println(balanceChecking(testValid));
         System.out.println(balanceChecking(testNonValid));
 

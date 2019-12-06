eeb63f6ae2954ba888b00f1e7f62c6b63a0dddf0
Aloa0001 <alex.oachesu0001@stud.hkr.se>
2019-12-06 13:52:18 +0100

    last updates

--- src/LogicMain.java
+++ src/LogicMain.java
@@ -20,8 +20,6 @@ public class LogicMain {
     private static StackWithOneQueue<Integer> myStackO = new StackWithOneQueue<>();
     private static AddressBook myAddressBook = new AddressBook();
 
-
-
     public static void main(String[] args) {
         seminarMenu();
     }
